006edf52b1e320881d3d757e4c6d184d3d3fbbf5
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2026-08-14 18:26:13 +0530

    feat: implement user app with initial API registration test case and project configuration

---
+++ app/user/test/test_user_api.py
@@ -0,0 +1,37 @@
+"""Test for the user api"""
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse("user:create")
+
+
+def create_user(**params):
+    """create and return new user."""
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserAPITests(TestCase):
+    """Test the public features of API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating user successful"""
+
+        payload = {
+            "email": "test@example.com",
+            "password": "test123",
+            "name": "Test Name",
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
