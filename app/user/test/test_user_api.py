e23c22468079aa9fe810d215947815d910962e14
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2026-08-17 05:05:47 +0530

    test: add password validation tests and verify sensitive data exclusion in user API responses

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -35,3 +35,17 @@ class PublicUserAPITests(TestCase):
 
         user = get_user_model().objects.get(email=payload["email"])
         self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test_password_too_short_error(self):
+        """Test if password is too short"""
+
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "name": "Test Name",
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        self.assertFalse(user_exists)
