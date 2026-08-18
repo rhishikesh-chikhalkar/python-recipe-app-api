3ff3d8421ed4f2a5e631115f37ebef1b009b14a9
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2026-08-18 19:41:01 +0530

    feat: implement user creation API endpoint with serializer and routing

--- app/user/views.py
+++ app/user/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
